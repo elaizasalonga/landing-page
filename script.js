cece4d5c801cd6ccef3b894136bc0526a5ca7ac4
elaizasalonga <155804585+elaizasalonga@users.noreply.github.com>
2024-03-04 02:21:48 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector('header nav');
+const footer = document.querySelector('footer');
+
+
+navbar.addEventListener('click', () => {
+
+});
+
+footer.addEventListener('click', () => {
+});
+
+
